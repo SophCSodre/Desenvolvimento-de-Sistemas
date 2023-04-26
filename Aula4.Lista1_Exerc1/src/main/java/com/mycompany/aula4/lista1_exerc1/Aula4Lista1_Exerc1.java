32ba98028eedb9827cfbbf54e26ef123f46a25d5
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 10:56:51 -0300

    Update Aula4Lista1_Exerc1.java

--- Aula4.Lista1_Exerc1/src/main/java/com/mycompany/aula4/lista1_exerc1/Aula4Lista1_Exerc1.java
+++ Aula4.Lista1_Exerc1/src/main/java/com/mycompany/aula4/lista1_exerc1/Aula4Lista1_Exerc1.java
@@ -8,6 +8,11 @@ import java.util.Scanner;
 public class Aula4Lista1_Exerc1 {
 
     public static void main(String[] args) {
+        //Crie uma Classe com o nome Teste1
+        //solicite ao usuário o nome , as notas de primeiro segundo e terceiro bimestre e de como resultado a seguinte informação :
+        //O Aluno (nome) teve como média a nota (média)
+
+        
         Scanner grava = new Scanner(System.in);
         
         double nota1 = 0.0;
@@ -21,11 +26,11 @@ public class Aula4Lista1_Exerc1 {
         System.out.println("Digite as notas:");
         nota1 = grava.nextDouble();
         nota2 = grava.nextDouble();
-        nota3 = grava.nextDouble();      
+        nota3 = grava.nextDouble();
         
         double media1 = nota1 + nota2 + nota3;
         double media2 = media1/3;
-       
-        System.out.println("O Aluno " + nome + " teve a media de: \n" + media2);
+        
+        System.out.println(nome + " teve como média: " + media2);
     }
 }
