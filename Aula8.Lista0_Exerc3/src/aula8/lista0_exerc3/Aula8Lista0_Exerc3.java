00da22df77729b90078e42994f1c28fbf79d8d2b
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:05:10 -0300

    média idade de N alunos

--- Aula8.Lista0_Exerc3/src/aula8/lista0_exerc3/Aula8Lista0_Exerc3.java
+++ Aula8.Lista0_Exerc3/src/aula8/lista0_exerc3/Aula8Lista0_Exerc3.java
@@ -6,25 +6,26 @@ import java.util.Scanner;
 public class Aula8Lista0_Exerc3 {
 
     public static void main(String[] args) {
-        Scanner grava = new Scanner(System.in);
-        double media = 0.0;
-        int ida, num, quant=0;
+        //Receba a idade de N alunos e forneça a média
         
-        System.out.println("Digite a quantidade de alunos");
+        Scanner grava = new Scanner(System.in);
+        
+        int idade, quant, num;
+        double media = 0;
+        
+        System.out.println("Digite a quantidade de alunos:");
         num = grava.nextInt();
         quant = num;
-
+        
         do{
-            System.out.println("Digite a idade dos alunos");
-            ida = grava.nextInt();
-            media = media + ida;
+            System.out.println("Digite a idade dos alunos:");
+            idade = grava.nextInt();
+            media = media + idade;
             quant = quant - 1;
-
-
-        }while(quant > 0);
+            
+        }while (quant > 0);
         
         media = media/num;
-        System.out.println("A média de idade dos alunos é de: " + media);
+        System.out.println("A idade média dos alunos é de: " + media);
     }
-    
 }
