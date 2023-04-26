f270edf373d266004923124d5c1b224b7a7f7b43
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:03:18 -0300

    exibindo n de 1 a 100 - do-while

--- Aula8.Lista0_Exerc2/src/aula8/lista0_exerc2/Aula8Lista0_Exerc2.java
+++ Aula8.Lista0_Exerc2/src/aula8/lista0_exerc2/Aula8Lista0_Exerc2.java
@@ -4,13 +4,14 @@ package aula8.lista0_exerc2;
 public class Aula8Lista0_Exerc2 {
 
     public static void main(String[] args) {
-        int num = 101, cont = 1;
+        //Crie um programa que exiba na tela os numeros de 100 ate 0 ;
+        
+        int num = 100, cont = 1;
         
         do{
             num = num - cont;
             System.out.println(num);
             
-        }while (num >= 1);
+        }while (0 < num);
     }
-    
 }
