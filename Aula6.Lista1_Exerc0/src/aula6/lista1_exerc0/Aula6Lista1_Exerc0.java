6d465c272fefe1a6d41306a8c5fb19c45ee6a696
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 11:25:17 -0300

    calculando imc if-else

--- Aula6.Lista1_Exerc0/src/aula6/lista1_exerc0/Aula6Lista1_Exerc0.java
+++ Aula6.Lista1_Exerc0/src/aula6/lista1_exerc0/Aula6Lista1_Exerc0.java
@@ -6,34 +6,35 @@ import java.util.Scanner;
 public class Aula6Lista1_Exerc0 {
 
     public static void main(String[] args) {
+        //Calcule o IMC e determine se esta:
+        //Abaixo do peso, Peso normal, Sobre peso, Obesidade leve, Obesidade moderada, Obesidade morbida
         Scanner grava = new Scanner(System.in);
-       
-        String nome = "";
-        System.out.println("Digite seu nome");
-        nome = grava.next();
         
-        double peso, alt, imc;
-        System.out.println("Digite peso");
+        double peso = 0, altura = 0, imc = 0;
+        
+        System.out.println("Digite seu peso e altura:");
         peso = grava.nextDouble();
-
-        System.out.println("Digite altura");
-        alt = grava.nextDouble();
+        altura = grava.nextDouble();
         
-        imc = peso/ (alt * alt);
+        imc = peso / (altura * altura);
         
         if (imc < 18.5){
             System.out.println("Abaixo do peso");
-        } else if (imc < 24.9) {
+        }
+        else if (imc < 24.9){
             System.out.println("Peso normal");
-        } else if (imc < 29.9) {
+        }
+        else if (imc < 29.9){
             System.out.println("Sobre peso");
-        } else if (imc < 34.9) {
+        }
+        else if (imc < 34.9){
             System.out.println("Obesidade leve");
-        } else if (imc < 38.9) {
+        }
+        else if (imc < 39.9){
             System.out.println("Obesidade moderada");
-        } else {
-        System.out.println("Obesidade morbida");
+        }
+        else {
+            System.out.println("Obesidade morbida");
         }
     }
-    
 }
