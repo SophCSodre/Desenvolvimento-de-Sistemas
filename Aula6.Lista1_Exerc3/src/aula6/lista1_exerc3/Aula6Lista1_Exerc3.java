9026f566619b20d53a97e8e60d4f8250f2e99f37
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 11:58:36 -0300

    exercicio if-else

--- Aula6.Lista1_Exerc3/src/aula6/lista1_exerc3/Aula6Lista1_Exerc3.java
+++ Aula6.Lista1_Exerc3/src/aula6/lista1_exerc3/Aula6Lista1_Exerc3.java
@@ -6,23 +6,30 @@ import java.util.Scanner;
 public class Aula6Lista1_Exerc3 {
 
     public static void main(String[] args) {
+        //CATEGORIAS:
+        //1 ATÉ 1O ANOS- INFANTIL
+        //11 ATE 13 ANOS - INFANTO-JUVENIL
+        //14 ATE 17 ANOS - PRE-ADOLESCENTE
+        //ACIMA DE 18 - ADULTO
+        
         Scanner grava = new Scanner(System.in);
         
-        int a = 0;
-        System.out.println("Digite sua idade:");
-        a = grava.nextInt();
+        int idade = 0;
         
-        if (a <= 0){
-            System.out.println("Idade inválida");
-        } else if (a <= 10){
-            System.out.println("Infatil");
-        } else if (a <= 13){
+        System.out.println("Digite sua idade:");
+        idade = grava.nextInt();
+        
+        if (idade <= 10){
+            System.out.println("Infantil");
+        }
+        else if (idade <= 13){
             System.out.println("Infanto-Juvenil");
-        } else if (a <= 17){
+        }
+        else if (idade <= 17){
             System.out.println("Adolescente");
-        } else {
+        }
+        else{
             System.out.println("Adulto");
         }
     }
-    
 }
