cb9fce73ccf598fec582aa812433c80c697f29bf
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 11:05:15 -0300

    tirando %, somando 10 e divindo 4

--- Aula4.Lista2_Exerc2/src/main/java/com/mycompany/aula4/lista2_exerc2/Aula4Lista2_Exerc2.java
+++ Aula4.Lista2_Exerc2/src/main/java/com/mycompany/aula4/lista2_exerc2/Aula4Lista2_Exerc2.java
@@ -6,18 +6,24 @@ import java.util.Scanner;
 public class Aula4Lista2_Exerc2 {
 
     public static void main(String[] args) {
+        //Solicite ao usuário um valor em reais 
+        //Desse valor tire 6% 
+        //Some ao resultado dessa operação o valor 10
+        //Divida por 4
+        //Mostre o resultado
+        
         Scanner grava = new Scanner(System.in);
         
-        double a, val1, val2, val3, val4;
+        double valor = 0, res1 = 0, res2 = 0, res3 = 0, res4 = 0;
         
-        System.out.println("Digite um valor em reais: ");
-        a = grava.nextDouble();
+        System.out.println("Digite o valor desejado:");
+        valor = grava.nextDouble();
         
-        val1 = 0.06 * a;
-        val2 = a - val1;
-        val3 = val2 + 10;
-        val4 = val3 / 4;
+        res1 = valor * 0.06;
+        res2 = valor - res1;
+        res3 = res2 + 10;
+        res4 = res3 / 4;
         
-        System.out.println("O resultado é: " + val4);
+        System.out.println("O resultado é: " + res4);
     }
 }
