6e8f793901773bfb36b645bf653a7319bc7c9ad0
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 11:03:28 -0300

    registrar 3 valores, soma e divide

--- Aula4.Lista2_Exerc1/src/main/java/com/mycompany/aula4/lista2_exerc1/Aula4Lista2_Exerc1.java
+++ Aula4.Lista2_Exerc1/src/main/java/com/mycompany/aula4/lista2_exerc1/Aula4Lista2_Exerc1.java
@@ -1,29 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.aula4.lista2_exerc1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author sophia
- */
 public class Aula4Lista2_Exerc1 {
 
     public static void main(String[] args) {
+        //Crie uma classe como o nome Soma 
+        //Solicite 3 valores ao usuário
+        //Some os 3 valores e multiplique por 10
+        //o resultado da multiplicação divida por 2
+        //Faça todas as operações em apenas 1(uma )linha
+        //Exiba o resultado
+        
         Scanner grava = new Scanner(System.in);
+        double valor1 = 0;
+        double valor2 = 0;
+        double valor3 = 0;
+        double res = 0;
         
-        double a=0, b=0, c=0, res=0;
+        System.out.println("Digite 3 valores");
+        valor1 = grava.nextDouble();
+        valor2 = grava.nextDouble();
+        valor3 = grava.nextDouble();
         
-        System.out.println("Digite 3 valores:");
-        a = grava.nextDouble();
-        b = grava.nextDouble();
-        c = grava.nextDouble();
-        
-        res = a + b + c * 10 / 2;
+        res = ((valor1 + valor2 + valor3) * 10) / 2;
         
         System.out.println("O resultado é: " + res);
     }
