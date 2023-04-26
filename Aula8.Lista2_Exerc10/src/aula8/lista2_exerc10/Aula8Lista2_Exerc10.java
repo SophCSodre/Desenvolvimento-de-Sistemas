7a69568088b7298a132fabff66265f2a3bab9cb0
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:08:50 -0300

    ler 3 valores e colocar em ordem crescente

--- Aula8.Lista2_Exerc10/src/aula8/lista2_exerc10/Aula8Lista2_Exerc10.java
+++ Aula8.Lista2_Exerc10/src/aula8/lista2_exerc10/Aula8Lista2_Exerc10.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Aula8Lista2_Exerc10 {
 
     public static void main(String[] args) {
+        //Ler três valores e colocá-los em ordem crescente.
+        
         Scanner gravar = new Scanner(System.in);
 		
 	int v1 = 1 , v2 = 2, v3 = 3, temp ;
@@ -16,25 +18,22 @@ public class Aula8Lista2_Exerc10 {
             temp =v1;
             v1= v2;
             v2 =temp;
-	
 					
 	} else if(v2>v3){
 			
             temp =v2;
             v2= v3;
             v3 =temp;
-	
-					
+				
 	}else if(v3 <v1){
 			
             temp =v3;
             v3= v1;
             v1 =temp;
-	
 					
 	}
 	} 
 	
 	System.out.println(v1 + " " + v2 + " " + v3);
-	}
+    }
     }
