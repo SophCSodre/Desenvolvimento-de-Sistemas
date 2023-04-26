ece5a8bd1324b96d57e6f7446f8ed68dd5bcabf5
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:14:35 -0300

    multiplos de 7 menores que 200

--- Aula8.Lista2_Exerc5/src/aula8/lista2_exerc5/Aula8Lista2_Exerc5.java
+++ Aula8.Lista2_Exerc5/src/aula8/lista2_exerc5/Aula8Lista2_Exerc5.java
@@ -4,6 +4,8 @@ package aula8.lista2_exerc5;
 public class Aula8Lista2_Exerc5 {
 
     public static void main(String[] args) {
+        //Imprimir os múltiplos de 7 menores que 200.
+        
         int num = 7 , cont =1 , res;
         
         do{
@@ -13,5 +15,4 @@ public class Aula8Lista2_Exerc5 {
             
         }while (res< 190);
     }
-    
 }
