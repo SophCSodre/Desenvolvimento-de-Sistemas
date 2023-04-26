200943f50fbe4951aa4c5765b0e8baf247b0cf3a
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 11:55:52 -0300

    simulação cadastro

--- Aula6.Lista1_Exerc1/src/aula6/lista1_exerc1/Aula6Lista1_Exerc1.java
+++ Aula6.Lista1_Exerc1/src/aula6/lista1_exerc1/Aula6Lista1_Exerc1.java
@@ -6,49 +6,44 @@ import java.util.Scanner;
 public class Aula6Lista1_Exerc1 {
 
     public static void main(String[] args) {
+        //CRIE UMA CLASSE QUE SIMULE O FUNCIONAMENTO DE UM CADASTRO DE CLIENTE:
+        //MENU: 1 - inclusão. 2 - alteracao. 3 - exclusao. 4 - sair. 
+        //Dados a serem cadastrados: CPF, RG, Código.
+        
         Scanner grava = new Scanner(System.in);
         
-        int res = 0;
-        int cpf, rg, cod;
+        int cpf = 0, rg = 0, cod = 0, res = 0;
         
-        System.out.println("MENU DE OPÇÕES");
-        System.out.println("1- Inclusão");
-        System.out.println("2- Alteração");
-        System.out.println("3- Exclusão");
-        System.out.println("4- Sair");
-        
+        System.out.println("MENU: \n1 - Inclusão \n2 - Alteração \n3 - Exlusão \n4 - Sair");
         res = grava.nextInt();
         
-        switch(res){
+        if (res == 1){
+            System.out.println("Digite seu CPF, RG e Código");
+            cpf = grava.nextInt();
+            rg = grava.nextInt();
+            cod = grava.nextInt();
             
-            case 1:
-                System.out.println("Digite seu RG");
-                rg = grava.nextInt();
-                System.out.println("Digite seu CPF");
-                cpf = grava.nextInt();
-                System.out.println("Digite seu Código");
-                cod = grava.nextInt();
-                break;
-                
-            case 2:
-               System.out.println("Digite seu RG");
-               rg = grava.nextInt();
-               System.out.println("Digite seu CPF");
-               cpf = grava.nextInt();
-               System.out.println("Digite seu Código");
-               cod = grava.nextInt();
-               break;
-               
-            case 3:
-                rg = 0;
-                cpf = 0;
-                cod = 0;
-                System.out.println("Registros apagados");
-                break;
-                
-            case 4:
-                System.out.println("Saindo do sistema");
+            System.out.println("INCLUIDO no banco de dados com SUCESSO!");
+        } 
+        else if (res == 2){
+            System.out.println("Digite seu CPF, RG e Código");
+            cpf = grava.nextInt();
+            rg = grava.nextInt();
+            cod = grava.nextInt();
+            
+            System.out.println("ALTERADO no banco de dados com SUCESSO!");
+        }
+        else if (res == 3){
+            cpf = 0;
+            rg = 0;
+            cod = 0;            
+            System.out.println("EXCLUIDO no banco de dados com SUCESSO!");
+        }
+        else if (res == 4){
+            System.out.println("SAINDO do sistema");
+        }
+        else{
+            System.out.println("Opção INVÁLIDA");
         }
     }
-    
 }
