09caf9f117fbb2b33bd6f353ffdc0cab644908e0
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:11:38 -0300

    multiplos de 3, entre 1 e 100

--- Aula8.Lista2_Exerc3/src/aula8/lista2_exerc3/Aula8Lista2_Exerc3.java
+++ Aula8.Lista2_Exerc3/src/aula8/lista2_exerc3/Aula8Lista2_Exerc3.java
@@ -4,6 +4,8 @@ package aula8.lista2_exerc3;
 public class Aula8Lista2_Exerc3 {
 
     public static void main(String[] args) {
+        //Imprima todos os múltiplos de 3, entre 1 e 100;
+        
         int num = 3 , cont =1 , res;
         
         do{
@@ -13,5 +15,4 @@ public class Aula8Lista2_Exerc3 {
             
         }while (res<100);
     }
-    
 }
