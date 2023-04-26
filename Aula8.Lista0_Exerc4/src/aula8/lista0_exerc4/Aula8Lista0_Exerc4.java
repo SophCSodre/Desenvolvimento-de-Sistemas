14fe80804ea32003b12ecd67d0b1cc531448b6eb
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:06:48 -0300

    aparecer mensagem X vezes

--- Aula8.Lista0_Exerc4/src/aula8/lista0_exerc4/Aula8Lista0_Exerc4.java
+++ Aula8.Lista0_Exerc4/src/aula8/lista0_exerc4/Aula8Lista0_Exerc4.java
@@ -6,16 +6,20 @@ import java.util.Scanner;
 public class Aula8Lista0_Exerc4 {
 
     public static void main(String[] args) {
+        //Receba via teclado quantas vezes a mensagem "Isso é um teste deve aparecer na tela "
+        
         Scanner grava = new Scanner(System.in);
-        int rec = 0;
         
-        System.out.println("Digite quantas vezes você quer que a mensagem apareça");
-        rec = grava.nextInt();
+        String mensagem = "Está é a mensagem para você";
+        int msg = 0;
         
-        do {
-            rec--;
-            System.out.println("Isso é um teste!");
-        }while (rec > 0);
+        System.out.println("Quantas vezes você quer ler a mensagem?");
+        msg = grava.nextInt();
+        
+        do{
+            msg--;
+            System.out.println(mensagem);
+            
+        }while (msg > 0);
     }
-    
 }
