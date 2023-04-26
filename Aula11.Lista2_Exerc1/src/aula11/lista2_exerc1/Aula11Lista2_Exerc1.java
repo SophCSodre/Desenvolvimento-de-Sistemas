82dd22bf125f4704002d99e9df20ae1745899ba3
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:21:41 -0300

    menu para cada exercicio

--- Aula11.Lista2_Exerc1/src/aula11/lista2_exerc1/Aula11Lista2_Exerc1.java
+++ Aula11.Lista2_Exerc1/src/aula11/lista2_exerc1/Aula11Lista2_Exerc1.java
@@ -6,7 +6,35 @@ import java.util.Scanner;
 public class Aula11Lista2_Exerc1 {
 
     public static void main(String[] args) {
-    Scanner grava = new Scanner(System.in);
+    //Crie um menu ( Executar e sair ) geral e crie um botão para cada exercicio
+        
+        //1- Criar um algoritmo que imprima uma tabela de conversão de polegadas para centímetros.
+        //Deseja-se que na tabela conste desde 1 polegada até 20 polegadas inteiras.
+        //Sabendo-se que a fórmula para conversão em centímetros é (polegadas*2,54)
+        
+        //2- A conversão de graus Fahrenheit para centígrados é obtida por C = 5*(F-32)/9.
+        //Fazer um algoritmo que calcule e escreva uma tabela de graus centígrados em função de graus 
+        //Fahrenheit, variando um a um de 50 a 150 graus Fahrenheit.    
+    
+        //3- Entrar com um nome, idade e sexo de 20 pessoas.
+        //Imprimir o nome se a pessoa for do sexo masculino e tiver mais de 21 anos
+        
+        // 4- Faça um algoritmo que receba 100 valores inteiros e positivos, e resolva as seguintes questões:  
+        //(a)  Calcule a raiz quadrada dos valores menores ou iguais a 100.
+        //(b) Para todo valor maior que 100, verifique se o mesmo é par, caso seja, imprimir o valor e a mensagem “valor par”.
+        //(c) Ao final, imprimir a quantidade de números pares maiores que 100.  
+    
+        //5- Entrar com sexo de várias pessoas e imprimir quantas pessoas são do sexo masculino.
+        //Estabeleça uma condição para saída do algoritmo.  
+     
+        //6- Dado um país A, com 5.000.000 de habitantes e uma taxa de natalidade de 3% ao ano, e um país  
+        //B com 7.000.000 de habitantes e uma taxa de natalidade de 2% ao ano, calcular e imprimir o 
+        //tempo necessário para que a população do país A ultrapasse a população do país B.  
+    
+        //7- João tem 1,50 m de altura e cresce 2 cm por ano, enquanto Maria tem 1,10 m e cresce 3 cm por ano.
+        //Construir um algoritmo que calcule e imprima quantos anos serão necessários para que Maria seja maior que João.
+        
+        Scanner grava = new Scanner(System.in);
     int op = 0;
         
     do {
@@ -87,6 +115,5 @@ public class Aula11Lista2_Exerc1 {
             System.out.println("Opção inválida");
         }
     }while (op != 7);
-    }
+    } 
 }
-
