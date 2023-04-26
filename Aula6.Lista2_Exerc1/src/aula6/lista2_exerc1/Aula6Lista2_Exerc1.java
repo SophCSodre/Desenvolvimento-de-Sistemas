e5242f8bbba84b629d7d67f83e3882caf24b6e85
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 12:57:44 -0300

    multas if-else

--- Aula6.Lista2_Exerc1/src/aula6/lista2_exerc1/Aula6Lista2_Exerc1.java
+++ Aula6.Lista2_Exerc1/src/aula6/lista2_exerc1/Aula6Lista2_Exerc1.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
 public class Aula6Lista2_Exerc1 {
 
     public static void main(String[] args) {
+        //SOLICITE AO USUÁRIO AS SEGUINTES INFORMAÇÕES :
+        //VELOCIDADE PERMITIDA NA VIA
+        //VELOCIDADE QUE O CARRO ESTA NO MOMENTO
+        //SE VELOCIDADE ESTIVER ACIMA DE  10KM DO PERMITIDO , O SISTEMA DARA UM AVISO DE "MULTA DE 80,00"
+        //SE A VELOCIDADE ESTIVER ACIMA ENTRE 11KM E 39 KM , O SISTEMA DA UM AVISO DE "MULTA DE 120,00"
+        //SE A VELOCIDADE FOR MAIOR OU IGUAL A 40 KM O SISTEMA DA UM AVISO DE "MULTA DE 200,00"
+        
         Scanner grava = new Scanner(System.in);
         
         double perm = 0.0;
@@ -20,13 +27,15 @@ public class Aula6Lista2_Exerc1 {
         
         if (media <= 0){
             System.out.println("Não há multa");
-        } else if (media <= 10){
+        } 
+        else if (media <= 10){
             System.out.println("Sua multa é de R$80,00");
-        } else if (media <= 39){
+        } 
+        else if (media <= 39){
             System.out.println("Sua multa é de R$120,00");
-        } else {
+        } 
+        else {
             System.out.println("Sua multa é de R$200,00");
         }
     }
-    
 }
