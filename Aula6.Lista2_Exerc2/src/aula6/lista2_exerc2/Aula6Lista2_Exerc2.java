c1f6358b303c305519ac9aeb79b27ade12d8b489
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 12:59:27 -0300

    vt, vr e ir

--- Aula6.Lista2_Exerc2/src/aula6/lista2_exerc2/Aula6Lista2_Exerc2.java
+++ Aula6.Lista2_Exerc2/src/aula6/lista2_exerc2/Aula6Lista2_Exerc2.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Aula6Lista2_Exerc2 {
 
     public static void main(String[] args) {
+        //SOLICITE AO USUARIO O VALOR DE SEU SALARIO , A SEGUIR EFETUE OS SEGUINTES DESCONTOS :
+        //2%  DE VALE TRANSPORTE, 6% DE TICKET REFEIÇÃO, 7% DE IMPOSTO DE RENDA 
+        //AO FINAL , SE O SALARIO LIQUIDO FOR MAIOR QUE 1200,00  EFETUE MAIS 15% DE DESCONTO DO 
+        //IMPOSTO DE RENDA E MOSTRE O SALARIO LIQUIDO FINAL  , CASO CONTRARIO
+        //MOSTRE O SALARIO JUNTO COM A MENSAGEM "NÃO HOUVE DESCONTO"
+
         Scanner grava = new Scanner(System.in);
         double vt, vr, ir, sal2;
         
@@ -24,7 +30,5 @@ public class Aula6Lista2_Exerc2 {
         } else {
             System.out.println("Não houve desconto.");
         }
-        
     }
-    
 }
