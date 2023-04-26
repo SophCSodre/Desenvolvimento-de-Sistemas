89984c999606bebeea696fbfae9d3c5789b03dab
Sophia C. Sodre <90869152+SophCSodre@users.noreply.github.com>
2023-04-26 13:18:44 -0300

    tabuada com for

--- Aula8.Lista2_Exerc9/src/aula8/lista2_exerc9/Aula8Lista2_Exerc9.java
+++ Aula8.Lista2_Exerc9/src/aula8/lista2_exerc9/Aula8Lista2_Exerc9.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Aula8Lista2_Exerc9 {
 
     public static void main(String[] args) {
+        //Imprimir a tabuada solicitada pelo usuario usando o FOR
+        
         Scanner grava = new Scanner(System.in);
         int num = 0;
         System.out.print("Digite a tabuada desejada: ");
@@ -15,7 +17,5 @@ public class Aula8Lista2_Exerc9 {
                 System.out.println(num*i);
             
             }
-            
-
-        }
     }
+}
